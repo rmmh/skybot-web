7a895676fad70a1aac4bc3c590e653194b9d620a
Alan Love <alan@dynamitealley.com>
2011-05-11 19:35:50 -0700

    added channel based views and some fancier templating

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config.from_pyfile('config.cfg')
 def connect_db():
     conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
-    return conn 
+    return conn
 
 
 @app.before_request
@@ -31,19 +31,33 @@ def after_request(response):
 @app.template_filter('ptime')
 def ptime(t):
     return time.strftime('%Y.%m.%d', time.gmtime(int(t)))
-    
+
 
 @app.route("/")
 def index():
     return "Hello World!"
 
 
-@app.route("/quotes")
-def list_quotes():
-    quotes = g.db.execute('SELECT * FROM quote WHERE deleted = 0 AND chan = ? '
-                          'order by nick asc, time asc', ('#cobol',)).fetchall()
+#@app.route("/quotes")
+#def list_quotes():
+#    quotes = g.db.execute('SELECT * FROM quote WHERE deleted = 0 AND chan = ? '
+#                          'order by nick asc, time asc', ('#cobol',)).fetchall()
+#
+#    chan_name = "#cobol"
+#    return render_template('list_quotes.html', quotes=quotes, chan_name=chan_name)
 
-    return render_template('list_quotes.html', quotes=quotes)
+@app.route("/quotes/")
+@app.route("/quotes/<chanName>")
+def list_quotes(chanName=None):
+    if chanName == None:
+        chan_name = "#cobol"
+    else:
+        chan_name = "#" + chanName
+
+    quotes = g.db.execute('SELECT * FROM quote WHERE deleted =0 AND chan= ? '
+                          'order by nick asc, time asc', (chan_name,)).fetchall()
+
+    return render_template('list_quotes.html', quotes=quotes, chan_name=chan_name)
 
 
 if __name__ == "__main__":
